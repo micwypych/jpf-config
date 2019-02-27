ac005b7058440228a42a9eab4daba346a84da132
Michał Wypych <mwypych@agh.edu.pl>
2019-02-27 15:13:27 +0100

    updates script to download artifacts from github

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ set -e
 mkdir jpf
 cd jpf
 
-wget http://babelfish.arc.nasa.gov/trac/jpf/raw-attachment/wiki/projects/jpf-core/jpf-core-r32.zip
-wget http://babelfish.arc.nasa.gov/trac/jpf/raw-attachment/wiki/projects/jpf-core/jpf-core-r32-src.zip
+wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-core-r32.zip
+wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-core-r32-src.zip
 unzip ./jpf-core-r32.zip
 echo 'An' | unzip ./jpf-core-r32-src.zip
 
@@ -16,16 +16,16 @@ echo -e '\n\n#JPF CORE' >> ${SITE_PROPERTIES}
 echo "jpf-core = ${PWD}/jpf-core" >> ${SITE_PROPERTIES}
 
 
-wget https://bitbucket.org/michelelombardi/jpf-ltl/get/08e687c362de.zip
-unzip ./08e687c362de.zip
+wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-ltl.zip
+unzip ./jpf-ltl.zip
 mv ./michelelombardi-jpf-ltl-08e687c362de ./jpf-ltl
 chmod u+x ./jpf-ltl/bin/jpf
 
 echo -e '\n\n#JPF LTL EXTENSION' >> ${SITE_PROPERTIES}
 echo "jpf-ltl = ${PWD}/jpf-ltl" >> ${SITE_PROPERTIES}
 
-wget http://babelfish.arc.nasa.gov/trac/jpf/raw-attachment/wiki/projects/jpf-aprop/jpf-aprop-r58.zip
-wget http://babelfish.arc.nasa.gov/trac/jpf/raw-attachment/wiki/projects/jpf-aprop/jpf-aprop-r58-src.zip
+wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-aprop-r58.zip
+wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-aprop-r58-src.zip
 unzip ./jpf-aprop-r58.zip
 echo 'An' | unzip ./jpf-aprop-r58-src.zip
 
