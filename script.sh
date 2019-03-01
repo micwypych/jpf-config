573e46bc8dd8b95b6fd502fe351464e8d213e561
Michał Wypych <mwypych@agh.edu.pl>
2019-03-01 14:37:39 +0100

    Removes unrequired packages from the script

--- script.sh
+++ script.sh
@@ -5,9 +5,7 @@ mkdir jpf
 cd jpf
 
 wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-core-r32.zip
-wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-core-r32-src.zip
 unzip ./jpf-core-r32.zip
-echo 'An' | unzip ./jpf-core-r32-src.zip
 
 SITE_PROPERTIES=./site.properties
 
@@ -15,25 +13,8 @@ echo '# JPF site configuration' > ${SITE_PROPERTIES}
 echo -e '\n\n#JPF CORE' >> ${SITE_PROPERTIES}
 echo "jpf-core = ${PWD}/jpf-core" >> ${SITE_PROPERTIES}
 
-
-wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-ltl.zip
-unzip ./jpf-ltl.zip
-mv ./michelelombardi-jpf-ltl-08e687c362de ./jpf-ltl
-chmod u+x ./jpf-ltl/bin/jpf
-
-echo -e '\n\n#JPF LTL EXTENSION' >> ${SITE_PROPERTIES}
-echo "jpf-ltl = ${PWD}/jpf-ltl" >> ${SITE_PROPERTIES}
-
-wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-aprop-r58.zip
-wget https://github.com/micwypych/jpf-artifacts/raw/master/jpf/jpf-aprop-r58-src.zip
-unzip ./jpf-aprop-r58.zip
-echo 'An' | unzip ./jpf-aprop-r58-src.zip
-
-echo -e '\n\n#JPF JAVA ANNOTAION EXTENSION' >> ${SITE_PROPERTIES}
-echo "jpf-aprop = ${PWD}/jpf-aprop" >> ${SITE_PROPERTIES}
-
-echo -e '\n\n'
-echo 'extensions=${jpf-core},${jpf-ltl},${jpf-aprop}' >> ${SITE_PROPERTIES}
+echo -e '\n\n' >> ${SITE_PROPERTIES}
+echo 'extensions=${jpf-core}' >> ${SITE_PROPERTIES}
 
 mkdir ~/.jpf
 touch ~/.jpf/site.properties
